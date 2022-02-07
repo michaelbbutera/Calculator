8071b24ac8549d4b5d3d995f1ff6470c7b453b99
michaelbutera <michael213@revature.net>
2022-02-07 18:08:29 -0500

    first commit for real

---
+++ src/Calculator.java
@@ -0,0 +1,47 @@
+//Michael Butera's Calculator exercise
+//2022 2 7
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int answer = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter First Number: ");
+        int firstNum = scanner.nextInt();
+
+        System.out.print("Enter Second Number: ");
+        int secondNum = scanner.nextInt();
+
+        System.out.println("Select Option:\n" +
+                "1 - Add\n" +
+                "2 - Subtract\n" +
+                "3 - Multiply\n" +
+                "4 - Divide");
+        int operator = scanner.nextInt();
+
+        switch(operator) {
+            case 1:
+                answer = firstNum + secondNum;
+                break;
+            case 2:
+                answer = firstNum - secondNum;
+                break;
+            case 3:
+                answer = firstNum * secondNum;
+                break;
+            case 4:
+                if(secondNum == 0) {
+                    System.out.println("Cannot divide by 0");
+                    break;
+                }
+                answer = firstNum / secondNum;
+                break;
+            default:
+
+        }
+
+        System.out.println("Result: " + answer);
+    }
+}
